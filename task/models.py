be267bb7c79e525b1ffa5932fbc60765a5a16251
DESKTOP-6AE2BR5\ANHKIET <nakietfit@gmail.com>
2019-06-01 17:42:14 +0700

    Attaching User to Task

--- task/models.py
+++ task/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Task(models.Model):
   name = models.TextField(blank=True)
+  logged_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
--- task/schema.py
+++ task/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 from .models import Task
+from user.schema import UserType
 
 class TaskType(DjangoObjectType):
   class Meta:
@@ -15,17 +16,24 @@ class Query(graphene.ObjectType):
 class CreateTask(graphene.Mutation):
   id = graphene.Int()
   name = graphene.String()
+  logged_by = graphene.Field(UserType)
 
   class Arguments:
     name = graphene.String()
 
   def mutate(self, info, name):
-    task = Task(name=name)
+    user = info.context.user or None
+
+    task = Task(
+      name=name,
+      logged_by=user,
+    )
     task.save()
 
     return CreateTask(
       id=task.id,
       name=task.name,
+      logged_by=task.logged_by,
     )
 
 class Mutation(graphene.ObjectType):
